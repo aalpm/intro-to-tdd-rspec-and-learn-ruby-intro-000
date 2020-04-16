7b991b6ef64d98f74979241eee9d8070048c7324
Avni Alp Mumcu <a.alp.mumcu@gmail.com>
2020-04-16 20:28:57 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,13 @@
 def current_age_for_birth_year(birth_year)
 2020 - birth_year
 
+puts "What year were you born?"
+birth_year = gets.to_i
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
+
+
+
 end
